ad7f4377ed77e449f5a3bbde9b12e29bd7e6461c
vaavaav <pedro_peixoto@outlook.pt>
2021-12-02 14:19:24 +0000

    Add AoC 2.1

---
+++ 2.hs
@@ -0,0 +1,16 @@
+import Data.List
+import System.Environment 
+
+f :: (Int,Int) -> (String, Int) -> (Int,Int)
+f (h,v) ("forward", n) = (h+n,v  )
+f (h,v) ("up", n )     = (h  ,v-n)
+f (h,v) ("down", n)    = (h  ,v+n)
+
+
+main = print 
+     . uncurry (*)
+     . foldl f (0,0)
+     . map ((\[s,n] -> (s, read n :: Int)) . words)
+     . lines
+     =<< readFile 
+     =<< head <$> getArgs 
