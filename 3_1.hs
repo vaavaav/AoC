3a32278cb97b933d5a47759b5d26e31da1de72a4
vaavaav <pedro_peixoto@outlook.pt>
2021-12-03 20:06:56 +0000

    Update AoC 3 - Part 1

--- 3_1.hs
+++ 3_1.hs
@@ -1,16 +1,18 @@
 import Data.List
 import System.Environment 
 
-
 solve :: [[Int]] -> Int
 solve = product
-      . foldl (\[m,l] (i,n) -> [m + n*2^i, l + (1-n)*2^i]) [0,0]
-      . zip [0..]
-      . reverse 
-      . map (\l -> fromEnum $ 2 * sum l >= length l)
+      . map sum
       . transpose
+      . zipWith (\i l -> [2^i * mcb l, 2^i * lcb l]) [0..]
+      . reverse 
+      . transpose
+      where mcb l = fromEnum $ 2 * sum l >= length l
+            lcb l = 1 - mcb l
+            
 
-parse :: String ->  [[Int]]
+parse :: String -> [[Int]]
 parse = map (map (read . pure)) . lines
 
 main = print . solve . parse =<< readFile =<< head <$> getArgs 
