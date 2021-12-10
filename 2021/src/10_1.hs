3e05b4f957627ffc2c3bdcdc33c482ea55047227
vaavaav <vaavaav.pt@gmail.com>
2021-12-10 13:21:15 +0000

    Add AoC 9 - Part 2 and incomplete AoC 10 - Part 1

---
+++ 2021/src/10_1.hs
@@ -0,0 +1,18 @@
+import Data.List
+import System.Environment 
+
+close :: Char -> Char
+close '[' = ']'
+close '(' = ')'
+close '{' = '}'
+close '<' = '>'
+
+
+
+getCorrupted = map (\l@(h:_) -> span (/= close h) l)
+
+solve = getCorrupted  
+
+parse = lines
+
+main = print . solve .  parse =<< readFile . head =<< getArgs 
