272404a7dc0c4261c281f4a9ade67e4b43c6314c
vaavaav <pedro_peixoto@outlook.pt>
2021-12-14 11:13:42 +0000

    Add incomplete otimization of AoC14

--- 2021/src/14_1.hs
+++ 2021/src/14_1.hs
@@ -1,32 +1,34 @@
 import Data.List
 import System.Environment 
-import Data.List.Split (splitOn)
+import Data.List.Split (splitOn, chunksOf)
 import Data.Bifunctor
-import Data.HashMap.Strict (HashMap, fromListWith, elems)
+import Data.HashMap.Strict (HashMap, fromListWith, toList, empty)
 
-subs :: [(String,Char)] -> String -> String
-subs _ ""  = ""
-subs _ [x] = [x] 
-subs l (h:s:t) = f l [h,s] ++ subs l (s:t)
- where f [] [a,b] = [a]
-       f ((h,o):t) x@[a,b] 
-         | h == x = [a,o]
-         | otherwise = f t x
+subs :: [(String,Char)] -> (HashMap String Int,HashMap Char Int) -> (HashMap String Int,HashMap Char Int)
+subs l (s,c) = fromListWith (+) $ concatMap (f l) $ toList s
+  where f [] x = [x]
+        f ((h,o):t) x@(k@[a,b],v)
+          | h == k = [([a,o],v),([o,b],v)]
+          | otherwise = f t x  
 
-step :: Int -> (String,[(String,Char)]) -> String
-step n (s,l) = iterate (subs l) s !! n
+step :: Int -> (HashMap String Int, HashMap Char Int) -> [(String,Char)] -> (HashMap String Int, HashMap Char Int)
+step n (s,c) l = iterate (subs l) (h,c) !! n
 
-solve :: Int -> (String, [(String,Char)]) -> Int
-solve n = (\l -> maximum l - minimum l) 
-        . elems
-        . fromListWith (+) 
-        . (`zip` repeat 1) 
-        . step n
+start :: String -> (HashMap String Int, HashMap Char Int)
+start l@(h:t) = (s,c)
+  where s = fromListWith (+) $ zipWith (\a b -> ([a,b],1)) l t
+        c = fromListWith (+) $ (`zip` repeat 1) l
+
+--solve :: Int -> (String, [(String,Char)]) -> Int
+solve n (s,l) = -- (\l -> maximum l - minimum l)
+              -- $ toList
+              snd
+              $ step n (start s)
+
 
 parse :: String -> (String, [(String,Char)])
 parse = bimap concat f . splitAt 1 . lines
  where f = map g . tail
        g = (\[[a,b],[o]] -> ([a,b],o)) . splitOn " -> "  
-
 
 main = print =<< (\(n:f:t) -> solve (read n) . parse <$> readFile f) =<< getArgs
