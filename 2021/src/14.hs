62971474020cd71d5519795272de8a9325835691
vaavaav <pedro_peixoto@outlook.pt>
2021-12-15 14:41:52 +0000

    Update AoC 14

--- 2021/src/14.hs
+++ 2021/src/14.hs
@@ -2,16 +2,16 @@ import Data.List
 import System.Environment 
 import Data.List.Split (splitOn)
 import Data.Bifunctor
-import Data.HashMap.Strict as H (HashMap, fromListWith, elems, unionWith, singleton, insertWith, toList, adjust, (!?))
+import Data.HashMap.Strict as H (HashMap, fromListWith, elems, unionWith, singleton, insertWith, toList, adjust, (!?), delete)
 
 type Dict = HashMap Char (HashMap Char Integer)
 
 subs :: [((Char,Char),Char)] -> Dict -> Dict
 subs [] h = h
-subs (((a,b),c):t) h = maybe id f ((!? b) =<< (h !? a)) $ subs t h
+subs (((a,b),c):t) h = maybe id f ((!? b) =<< (h !? a)) 
+                     $ subs t (adjust (H.delete b) a h)
  where f n = insertWith (unionWith (+)) a (singleton c n)
            . insertWith (unionWith (+)) c (singleton b n)
-           . adjust (adjust (+(-n)) b) a
 
 step :: Int -> Dict -> [((Char,Char),Char)] -> Dict
 step n h l = iterate (subs l) h !! n
